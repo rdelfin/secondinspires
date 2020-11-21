b2e4a80d81807ec2189de49bde6a3f61c2cf35c0
Ricardo Delfin <me@rdelfin.com>
2020-11-21 14:58:15 +0000

    Ran and setup prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import React, { FC } from 'react';
-import { Layout, Menu, Breadcrumb, PageHeader } from 'antd';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
-import './App.less';
+import React, { FC } from "react";
+import { Layout, Menu, Breadcrumb, PageHeader } from "antd";
+import {
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+} from "@ant-design/icons";
+import "./App.less";
 
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
@@ -10,7 +14,7 @@ const App: FC = () => (
   <Layout className="layout">
     <Header>
       <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
         <Menu.Item key="1">nav 1</Menu.Item>
         <Menu.Item key="2">nav 2</Menu.Item>
         <Menu.Item key="3">nav 3</Menu.Item>
@@ -20,9 +24,9 @@ const App: FC = () => (
       <Sider width={200} className="site-layout-background">
         <Menu
           mode="inline"
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          style={{ height: '100%', borderRight: 0 }}
+          defaultSelectedKeys={["1"]}
+          defaultOpenKeys={["sub1"]}
+          style={{ height: "100%", borderRight: 0 }}
         >
           <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
             <Menu.Item key="1">option1</Menu.Item>
@@ -44,23 +48,23 @@ const App: FC = () => (
           </SubMenu>
         </Menu>
       </Sider>
-      <Layout style={{ padding: '0 24px 24px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
+      <Layout style={{ padding: "0 24px 24px" }}>
+        <Breadcrumb style={{ margin: "16px 0" }}>
           <Breadcrumb.Item>Home</Breadcrumb.Item>
         </Breadcrumb>
         <Content
           className="site-layout-background"
           style={{
             padding: 24,
-              margin: 0,
-              minHeight: 280,
+            margin: 0,
+            minHeight: 280,
           }}
         >
-            <PageHeader
-              className="site-page-header"
-              title="Welcome"
-              subTitle="to SECOND robotics"
-            />
+          <PageHeader
+            className="site-page-header"
+            title="Welcome"
+            subTitle="to SECOND robotics"
+          />
         </Content>
       </Layout>
     </Layout>
